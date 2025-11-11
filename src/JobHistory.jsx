db72805882922d694918165c34e47de911b5bea6
Walibara <56965416+Walibara@users.noreply.github.com>
2025-11-11 10:12:18 -0800

    small minima changes

--- src/JobHistory.jsx
+++ src/JobHistory.jsx
@@ -29,7 +29,7 @@ export default function JobHistory() {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    return `${year}-${month}-${day}`;
   }
 
   //calculateing the total pages
