4bc3188697c5b77d43cad2731bb039dd3ce4e43d
CIELOARGE <158418246+CIELOARGE@users.noreply.github.com>
2026-04-07 23:54:39 -0700

    Changes for cognito and db

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -5,6 +5,9 @@ import {
   Link,
 } from 'react-router-dom'
 
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react'
+
 import './App.css'
 import hpLogo from '../../assets/HP_logo_2012.svg'
 import AmplifyHome from '../AmplifyHome/AmplifyHome.jsx'// we can remove this later
@@ -20,73 +23,97 @@ import FileRendering from '../FileRendering/FileRendering.jsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        {/* Sidebar */}
-        <aside className="sidebar">
-          <Link to="/dashboard">
-            <img
-              src={hpLogo}
-              alt="HP Logo"
-              className="hp-logo"
-              style={{ cursor: "pointer" }}
-            />
-          </Link>
-
-          <nav className="nav-menu">
-            <Link to="/" className="nav-item">
-              Amplify Home
+    <Authenticator
+      signUpAttributes={['email']}
+      formFields={{
+        signUp: {
+          username: {
+            order: 1,
+            placeholder: 'Choose a username',
+          },
+          email: {
+            order: 2,
+            placeholder: 'Enter your email',
+          },
+          password: {
+            order: 3,
+          },
+          confirm_password: {
+            order: 4,
+          },
+        },
+      }}
+>
+      {({ signOut, user }) => ( 
+      <BrowserRouter>
+        <div className="app-container">
+          {/* Sidebar */}
+          <aside className="sidebar">
+            <Link to="/dashboard">
+              <img
+                src={hpLogo}
+                alt="HP Logo"
+                className="hp-logo"
+                style={{ cursor: "pointer" }}
+              />
             </Link>
 
-            <Link to="/my-account" className="nav-item">
-              My Account
-            </Link>
+            <nav className="nav-menu">
+              <Link to="/" className="nav-item">
+                Amplify Home
+              </Link>
 
-            <Link to="/dashboard" className="nav-item">
-              Dashboard
-            </Link>
+              <Link to="/my-account" className="nav-item">
+                My Account
+              </Link>
 
-            <div className="nav-item">My Jobs</div>
-            <ul className="nav-submenu">
-              <li>
-                <Link to="/file-upload">Job Submission</Link>
-              </li>
-              <li>
-                <Link to="/my-jobs/status">Job Status</Link>
-              </li>
-              <li>
-                <Link to="/my-jobs/history">Job History</Link>
-              </li>
-            </ul>
+              <Link to="/dashboard" className="nav-item">
+                Dashboard
+              </Link>
 
-            <div className="logout">Log out</div>
-          </nav>
-        </aside>
+              <div className="nav-item">My Jobs</div>
+              <ul className="nav-submenu">
+                <li>
+                  <Link to="/file-upload">Job Submission</Link>
+                </li>
+                <li>
+                  <Link to="/my-jobs/status">Job Status</Link>
+                </li>
+                <li>
+                  <Link to="/my-jobs/history">Job History</Link>
+                </li>
+              </ul>
 
-        {/* Main content */}
-        <main className="main-content">
-          <header className="topbar">
-            <span>(Presentation Demo - Backend not functional yet)</span>
-          </header>
+              <button onClick={signOut} className="logout">Log out</button>
+            </nav>
+          </aside>
 
-          <section className="content">
-            <Routes>
-              <Route path="/" element={<AmplifyHome />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/my-account" element={<MyAccount />} />
-              <Route path="/my-jobs" element={<MyJobs />} />
-              <Route path="/my-jobs/status" element={<JobStatus />} />
-              <Route path="/my-jobs/history" element={<JobHistory />} />
-              <Route path="/my-jobs/submit" element={<JobSubmission />} />
-              <Route path="/job-submission" element={<JobSubmission />} />
-              <Route path="/file-upload" element={<FileUpload/>}/>
-              <Route path="/file-rendering" element={<FileRendering/>}/> 
+          {/* Main content */}
+          <main className="main-content">
+            <header className="topbar">
+              <span>(Presentation Demo - Backend not functional yet)</span>
+            </header>
 
-            </Routes>
-          </section>
-        </main>
-      </div>
-    </BrowserRouter>
+            <section className="content">
+              <Routes>
+                <Route path="/" element={<AmplifyHome />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/my-account" element={<MyAccount />} />
+                <Route path="/my-jobs" element={<MyJobs />} />
+                <Route path="/my-jobs/status" element={<JobStatus />} />
+                <Route path="/my-jobs/history" element={<JobHistory />} />
+                <Route path="/my-jobs/submit" element={<JobSubmission />} />
+                <Route path="/job-submission" element={<JobSubmission />} />
+                <Route path="/file-upload" element={<FileUpload/>}/>
+                <Route path="/file-rendering" element={<FileRendering/>}/> 
+
+              </Routes>
+            </section>
+          </main>
+        </div>
+      </BrowserRouter>
+  )}
+  </Authenticator>
   )
 }
 
